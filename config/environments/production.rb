10ae07ccd465ba292ee54ac2ecbe433ef86c7c18
Yuri <ycr.data@gmail.com>
2024-01-02 15:45:34 -0300

    configuring production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -92,4 +92,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: https }
+
+  config.action_mailer.smtp_settings = {
+    address: Rails.application.credentials.dig(:aws, :ses_server),
+    port: 587,
+    user_name: Rails.application.credentials.dig(:aws, :ses_username),
+    password: Rails.application.credentials.dig(:aws, :ses_password),
+    authentication: :login,
+    enable_starttls_auto: true
+  }
 end
