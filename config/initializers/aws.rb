1d68dd8865ad9925e3193f3062f87450e9ba07a9
Yuri <ycr.data@gmail.com>
2024-01-02 22:10:47 -0300

    removing useless file

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-  region: 'us-east-1',
-  credentials: Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key)),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket('citizen-app-pics')
