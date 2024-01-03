ff1c561be9d5de65e0dfbf95e69d86f3f0a0e6a3
Yuri <ycr.data@gmail.com>
2024-01-02 22:06:44 -0300

    production configuration for s3 and ses

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,4 +3,4 @@ Aws.config.update({
   credentials: Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key)),
 })
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+S3_BUCKET = Aws::S3::Resource.new.bucket('citizen-app-pics')
