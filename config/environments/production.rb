519bcd98832ff9d2845fd4bc40edf4da57414dad
Yuri <ycr.data@gmail.com>
2024-01-02 22:09:39 -0300

    fixing error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,8 +96,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { 
-    host: 'citizen-registration-app-71c8634ce7c2.herokuapp.com/', 
-    protocol: https 
+    host: 'https://citizen-registration-app-71c8634ce7c2.herokuapp.com/',  
   }
 
   config.action_mailer.smtp_settings = {
